0fd5badf08313a23d83e74c54c4c785abaf204db
zharif hadi <zharif.hadi@123rf.com>
2018-09-10 01:10:15 +0800

    First Commit

--- MotionEffectAnimation/ViewController.swift
+++ MotionEffectAnimation/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        applyMotionEffect(toView: self.backgroundImageView, withMagnitude: 10)
+        applyMotionEffect(toView: self.logoImageView, withMagnitude: -20)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func applyMotionEffect(toView: UIView, withMagnitude: Float) {
+        
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = -withMagnitude
+        xMotion.maximumRelativeValue = withMagnitude
+        
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        yMotion.minimumRelativeValue = -withMagnitude
+        yMotion.maximumRelativeValue = withMagnitude
+        
+        let groupMotion = UIMotionEffectGroup()
+        groupMotion.motionEffects = [xMotion, yMotion]
+        
+        self.view.addMotionEffect(groupMotion)
     }
 
 
